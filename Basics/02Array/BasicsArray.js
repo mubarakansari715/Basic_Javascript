6ed7ba40f3393da19274f047ed7480ca25c07728
Mubarak Ansari <54958497+mubarakansari715@users.noreply.github.com>
2024-10-04 10:00:29 +0000

    update array

---
+++ Basics/02Array/BasicsArray.js
@@ -0,0 +1,59 @@
+//Java script Array Practice
+
+const MyArray1 = [1, 2, 3, 4, 5];
+const MyArray2 = [6, 7, 8, 9];
+
+//check array
+// console.log(MyArray1);
+
+//shallow copy and deep copy
+
+/*
+shallow copy : which having the share same refrence of the copy array object. If you do some changes in copy object that will also 
+effecting to your origanal array.
+*/
+
+/*
+deep copy : which having the not share same refrence of the copy array object. If you do some changes in copy object that will not 
+effecting to your origanal array.
+*/
+
+//=>Methods of Array
+
+//push
+MyArray1.push(12);
+//console.log(MyArray1);
+
+//pop / shift => remove last element
+MyArray1.pop();
+//console.log(MyArray1);
+
+//unshift -> add element in zero index
+MyArray1.unshift(15);
+//console.log(MyArray1);
+
+//shift
+MyArray1.shift();
+//console.log(MyArray1);
+
+//includes => checked element exist in array or not
+//console.log(MyArray1.includes(5))
+//console.log(MyArray1.indexOf(50))
+
+//join => make array into string
+//const newArray = MyArray1.join()
+//console.log(newArray);
+
+//slice => is not manipulation of origneal array
+const mySlice = MyArray1.slice(1,3)
+// console.log(mySlice);
+// console.log(MyArray1);
+
+//splice => is manipulation of origneal array, range will remove from the array
+const mySplice = MyArray1.splice(1,3)
+// console.log(mySplice);
+// console.log(MyArray1);
+
+
+
+
--- Basics/Array/BasicsArray.js
+++
@@ -1,8 +0,0 @@
-const MyArray1 = [1, 2, 3, 4, 5];
-const MyArray2 = [6, 7, 8, 9];
-
-console.log(MyArray1);
-
-console.log([...MyArray1, ...MyArray2])
-console.log([...MyArray1, ...MyArray2])
-
